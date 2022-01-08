6f4bc90920ec5f3f05e3e4737b8a00a68d63e22d
rajan <rajanranjan06254@gmail.com>
2022-01-08 09:11:43 +0530

    second_commit

--- connection.php
+++ connection.php
@@ -1,4 +1,14 @@
 <?php
 echo "hello world";
+$hostname="localhost";
+$username="root";
+$password="Rajan@#123";
+$dbname="rajan";
+$conn=mysqli_connect($hostname,$username,$password,$dbname);
+if($conn){
+    echo "you are successfully connected";
+}else{
+    echo "you are not connected due to " . mysqli_connect_error();
+}
 
 ?>
